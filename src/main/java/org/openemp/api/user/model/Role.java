4a5240d406d122aef6b379628d7f9bad25ec5f9f
Djihad BENGATI <d.bengati@gmail.com>
2020-06-04 15:46:08 +0200

    [PRIVILEGES] :sparkles: Add user Privileges

--- src/main/java/org/openemp/api/user/model/Role.java
+++ src/main/java/org/openemp/api/user/model/Role.java
@@ -5,8 +5,8 @@ import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.Getter;
@@ -23,12 +23,17 @@ public class Role extends BaseEntity {
 	private static final long serialVersionUID = -6132338183111628177L;
 
 	private String name;
-
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "roles_privileges", joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "privilege_id", referencedColumnName = "id"))
-	private Set<Privilege> privileges = new HashSet<>();
-
-	@ManyToMany(mappedBy = "roles")
-	private Set<Profile> profiles = new HashSet<>();
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "roles_privileges", 
+        joinColumns = @JoinColumn(
+          name = "role_id", referencedColumnName = "id"), 
+        inverseJoinColumns = @JoinColumn(
+          name = "privilege_id", referencedColumnName = "id"))
+    private Set<Privilege> privileges = new HashSet<>();
+    
+    @ManyToMany(mappedBy = "roles")
+    private Set<Profile> profiles = new HashSet<>();
 
 }
