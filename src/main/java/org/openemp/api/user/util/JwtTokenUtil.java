db252445b0e22136109c7eb86bdf6f7b299a63fb
Djihad BENGATI <d.bengati@gmail.com>
2020-06-05 17:50:14 +0200

    [AUTHENTICATE] :sparkles: Manage "Remember me" functionality.

--- src/main/java/org/openemp/api/user/util/JwtTokenUtil.java
+++ src/main/java/org/openemp/api/user/util/JwtTokenUtil.java
@@ -1,7 +1,5 @@
 package org.openemp.api.user.util;
 
-import static org.openemp.api.user.util.Constant.JWT_TOKEN_VALIDITY;
-
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
@@ -24,13 +22,12 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Component
 public class JwtTokenUtil implements Serializable {
 
-    private static final long serialVersionUID = -2550185165626007488L;
+	private static final long serialVersionUID = -2550185165626007488L;
 
+	@Value("${jwt.secret}")
+	private String JWT_KEY;
 
-    @Value("${jwt.secret}")
-    private String JWT_KEY;
-    
-    private static final String AUTHORITIES_KEY = "auth";
+	private static final String AUTHORITIES_KEY = "auth";
 
 	/**
 	 * Gets username from token.
@@ -38,9 +35,9 @@ public class JwtTokenUtil implements Serializable {
 	 * @param token the token
 	 * @return the username from token
 	 */
-    public String getUsernameFromToken(String token) {
-        return getClaimFromToken(token, Claims::getSubject);
-    }
+	public String getUsernameFromToken(String token) {
+		return getClaimFromToken(token, Claims::getSubject);
+	}
 
 	/**
 	 * Gets expiration date from jwt token.
@@ -49,9 +46,9 @@ public class JwtTokenUtil implements Serializable {
 	 * @return the expiration date from token
 	 */
 	//
-    public Date getExpirationDateFromToken(String token) {
-        return getClaimFromToken(token, Claims::getExpiration);
-    }
+	public Date getExpirationDateFromToken(String token) {
+		return getClaimFromToken(token, Claims::getExpiration);
+	}
 
 	/**
 	 * Gets claim from token.
@@ -62,20 +59,20 @@ public class JwtTokenUtil implements Serializable {
 	 * @return the claim from token
 	 */
 	public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = getAllClaimsFromToken(token);
-        return claimsResolver.apply(claims);
-    }
+		final Claims claims = getAllClaimsFromToken(token);
+		return claimsResolver.apply(claims);
+	}
 
-    //for retrieving any information from token we will need the secret key
-    private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(JWT_KEY).parseClaimsJws(token).getBody();
-    }
+	// for retrieving any information from token we will need the secret key
+	private Claims getAllClaimsFromToken(String token) {
+		return Jwts.parser().setSigningKey(JWT_KEY).parseClaimsJws(token).getBody();
+	}
 
-    //check if the token has expired
-    private Boolean isTokenExpired(String token) {
-        final Date expiration = getExpirationDateFromToken(token);
-        return expiration.before(new Date());
-    }
+	// check if the token has expired
+	private Boolean isTokenExpired(String token) {
+		final Date expiration = getExpirationDateFromToken(token);
+		return expiration.before(new Date());
+	}
 
 	/**
 	 * Generate token string.
@@ -84,27 +81,33 @@ public class JwtTokenUtil implements Serializable {
 	 * @return the string
 	 */
 	//generate token for user
-    public String generateToken(UserDetails userDetails) {
+    public String generateToken(UserDetails userDetails, boolean rememberMe) {
     	String authorities = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority)
 				.collect(Collectors.joining(","));
 
         Map<String, Object> claims = new HashMap<>();
         claims.put(AUTHORITIES_KEY, authorities);
         
-        return doGenerateToken(claims, userDetails.getUsername());
+        Date expiration = new Date(System.currentTimeMillis() + Constant.JWT_TOKEN_VALIDITY_WITHOUT_REMEMEBERME);
+        
+        if(rememberMe) {
+        	expiration = new Date(System.currentTimeMillis() + Constant.JWT_TOKEN_VALIDITY_WITH_REMEMBERME); 
+        }
+        
+        return doGenerateToken(claims, userDetails.getUsername(), expiration);
     }
 
-    //while creating the token -
-    //1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
-    //2. Sign the JWT using the HS512 algorithm and secret key.
-    //3. According to JWS Compact Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
-    //   compaction of the JWT to a URL-safe string
-    private String doGenerateToken(Map<String, Object> claims, String subject) {
+	// while creating the token -
+	// 1. Define claims of the token, like Issuer, Expiration, Subject, and the ID
+	// 2. Sign the JWT using the HS512 algorithm and secret key.
+	// 3. According to JWS Compact
+	// Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
+	// compaction of the JWT to a URL-safe string
+	private String doGenerateToken(Map<String, Object> claims, String subject, Date expiration) {
 
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
-                .signWith(SignatureAlgorithm.HS512, JWT_KEY).compact();
-    }
+		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(expiration).signWith(SignatureAlgorithm.HS512, JWT_KEY).compact();
+	}
 
 	/**
 	 * Validate token.
@@ -113,8 +116,8 @@ public class JwtTokenUtil implements Serializable {
 	 * @param userDetails the user details
 	 * @return the boolean
 	 */
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = getUsernameFromToken(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = getUsernameFromToken(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
 }
--- src/test/java/org/openemp/api/user/OpenEMPSecurityTests.java
+++ src/test/java/org/openemp/api/user/OpenEMPSecurityTests.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openemp.api.user.controller.UserController;
@@ -27,7 +29,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringBootTest(classes = OpenempUserApiApplication.class)
 public class OpenEMPSecurityTests {
-	
+
 	@Autowired
 	UserService userService;
 
@@ -58,7 +60,7 @@ public class OpenEMPSecurityTests {
 	private static final String APPLICATION_JSON_VALUE = "application/json";
 
 	private MockMvc mockMvc;
-	
+
 	@BeforeEach
 	public void setup() {
 		UserController userController = new UserController(userService, authenticationManager, jwtTokenUtil,
@@ -71,7 +73,7 @@ public class OpenEMPSecurityTests {
 
 		// Generate a token with user name.
 		JwtResponse tokenWithProfiles = new JwtResponse(
-				jwtTokenUtil.generateToken(userDetailsService.loadUserByUsername("admin")));
+				jwtTokenUtil.generateToken(userDetailsService.loadUserByUsername("admin"), false));
 
 		// Request authentication.
 		JwtRequest login = new JwtRequest();
@@ -91,7 +93,39 @@ public class OpenEMPSecurityTests {
 
 		// Generate a token with user name.
 		JwtResponse tokenWithoutProfiles = new JwtResponse(
-				jwtTokenUtil.generateToken(userDetailsService.loadUserByUsername("admin")));
+				jwtTokenUtil.generateToken(userDetailsService.loadUserByUsername("admin"), false));
 		assertThat(tokenWithProfiles.toString()).isNotEqualTo(tokenWithoutProfiles.toString());
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testAuthorizeWithRememberMe() throws Exception {
+		// Generate a token with user name.
+		JwtResponse tokenWithoutRemeberme = new JwtResponse(
+				jwtTokenUtil.generateToken(userDetailsService.loadUserByUsername("admin"), false));
+
+		// Generate a token with user name.
+		JwtResponse tokenWithRemeberme = new JwtResponse(
+				jwtTokenUtil.generateToken(userDetailsService.loadUserByUsername("admin"), true));
+		
+		// Tokens not null.
+		assertThat(tokenWithoutRemeberme).isNotNull();
+		assertThat(tokenWithRemeberme).isNotNull();
+		
+		// Getting expiration dates from tokens.
+		Date expirationWithoutRememberme = jwtTokenUtil.getExpirationDateFromToken(tokenWithoutRemeberme.getToken());
+		Date expirationWithRememberme = jwtTokenUtil.getExpirationDateFromToken(tokenWithRemeberme.getToken());
+		
+		assertThat(expirationWithoutRememberme).isNotNull();
+		assertThat(expirationWithRememberme).isNotNull();
+		assertThat(expirationWithoutRememberme).isBefore(expirationWithRememberme);
+		assertThat(expirationWithRememberme).isAfter(expirationWithoutRememberme);
+		
+		// Check the 7 days for the token with the remember me option.
+		Date dateToTest = new Date(System.currentTimeMillis() + Constant.JWT_TOKEN_VALIDITY_WITH_REMEMBERME);
+		
+		assertThat(dateToTest.getDay()).isEqualTo(expirationWithRememberme.getDay());
+		assertThat(dateToTest.getMonth()).isEqualTo(expirationWithRememberme.getMonth());
+		assertThat(dateToTest.getYear()).isEqualTo(expirationWithRememberme.getYear());
 	}
 }
